7c28ded32cad94d0e3decc605341b0de20db45c9
Mattew <stanislaus.m21@mhs.istts.ac.id>
2024-01-11 10:59:52 +0700

    topup sukses

--- app/Models/topup.php
+++ app/Models/topup.php
@@ -8,17 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class topup extends Model
 {
     use HasFactory;
-    public $table   = "users";
+    public $table   = "top_up";
     public $primaryKey = "id";
     public $incrementing = true;
     public $timestamps = false;
-    public $fillable = ['id','Tanggaltop_up','Status','Nominal','Bukti','id_user'];
+    public $fillable = ['id','tanggaltop_up','Status','Nominal','Bukti','id_user'];
     public function add($status,$nominal,$id_user)
     {
         $new = new topup();
         $new->Status = $status;
         $new->Nominal = $nominal;
-        $new->id_user = $id_user;
+        $new->id_user= $id_user;
         $new->save();
     }
 }
